abbbe858be5cef699c4fc44df9a6755aeca0b971
wi21b067 <wi21b067@technikum-wien.at>
2022-06-22 19:48:41 +0200

    Initial commit

---
+++ src/main/java/com/example/slm_abschlusspruefung/SlmAbschlusspruefungApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_abschlusspruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmAbschlusspruefungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmAbschlusspruefungApplication.class, args);
+    }
+
+}
